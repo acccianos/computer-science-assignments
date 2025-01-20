b222628eee118a9e0cd90553ac1c6d61386fc697
acccianos <20cosullivan.acc@lwetb.ie>
2025-01-20 13:23:48 +0000

    Add files via upload

---
+++ homework exercise.py
@@ -0,0 +1,122 @@
+temp = 123
+if temp < 32:
+    print ("ice")
+elif temp < 212:
+    print ("water")
+else:
+    print ("steam")
+    
+print()
+
+x = 1
+if x > 3:
+    if x > 4:
+        print ("A", end = ' ')
+    else:
+        print ("B", end = ' ')
+elif x < 2:
+    if (x != 0):
+        print ("C", end = ' ')
+print ("D")
+
+print()
+
+weather = 'raining'
+if weather == 'sunny':
+    print ("wear sunblock")
+elif weather == "snow":
+    print ("going skiing")
+else:
+    print (weather)
+
+print()
+
+if int('zero') == 0:
+    print ("zero")
+elif str(0) == 'zero':
+    print (0)
+elif str(0) == '0' :
+    print (str(0))
+else:
+    print ("none of the above")
+
+print()
+
+if n == 0
+    print ("zero")
+elif: n == 1
+    print ("one")
+elif
+    n == 2:
+        print ("two")
+else n == 3:
+    print ("three")
+
+print()
+
+n = int (input("Enter an integer:" ))
+if n < 1 :
+    print ("invalid value")
+else:
+    for i in range(1, n + 1 ) :
+        print (i * i)
+
+print()
+
+n = int (input("Enter an integer:" ))
+    if n < 1 :
+        print ("invalid value")
+        else:
+            for i in range(1, n + 1 ) :
+                print (i * i)
+
+print()
+
+n = int (input("Enter an integer:" ))
+if n < 1 :
+    print ("invalid value")
+else:
+    for i in range(1, n + 1 ) :
+        print (i * i)
+
+print()
+
+n = int (input("Enter an integer:" ))
+    if n > 0 :
+        for a in range(1, n + n) :
+            print (a / (n/2))
+    else:
+        print ('Now quitting')
+
+print()
+
+n = int (input("Enter an integer:" ))
+if n > 0 :
+    for a in range(1, n + n) :
+        print (a / (n/2))
+else:
+    print ('Now quitting')
+
+print()
+
+for i in range(100, 0, -3):
+    print(i)
+
+print()
+
+for i in range(4):
+    for j in range(5):
+        if i + 1 == j or j + 1 == 4:
+            print('+', end = ' ')
+        else:
+            print('o', end = ' ')
+
+print()
+
+count = 0
+a = int(input('Enter a value: '))
+while a != 0:
+    count = count + 1
+    a = int(input('Enter a value: '))
+print('You entered', count, 'values')
+
